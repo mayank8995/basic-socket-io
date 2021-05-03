def5ebb0fa544253edeb7f63454242c4725f2185
Mayank Gupta <mayankgupta8995@gmail.com>
2021-05-03 16:16:18 +0530

    spelling fixed commit

--- index.js
+++ index.js
@@ -7,24 +7,14 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello World</h1>');
-    // console.log(__dirname)
     res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
-    console.log('a user connected', socket);
-    // socket.on('chat message', () => {
-    //     io.emit('chat message', 'a user connected.')
-    // });
+    console.log('a user connected');
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
         io.emit('chat message', msg);
     });
-
-    // socket.on('disconnect', () => {
-    //     console.log('user disconnected');
-    // })
-
 });
 server.listen(3000, () => {
     console.log('listening at 3000');
